ee52d01be9a92ec199597e08a710db8e8d96e645
netomoura18@gmail.com <Neto Moura>
2025-08-11 15:59:52 -0300

    cria projeto ios no xcode

---
+++ NewsApp/NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Neto Moura on 11/08/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
