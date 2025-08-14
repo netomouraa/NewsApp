d61485b1295ce94e65ca4c1fc203c575ca1fe142
Neto Moura <Neto Moura>
2025-08-14 16:59:51 -0300

    adiciona "fetchFeed" para chamar API e listar notícias

--- NewsFeed/NewsFeed/Services/NewsFeedService.swift
+++ NewsFeed/NewsFeed/Services/NewsFeedService.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import Combine
+
+class NewsFeedService: ObservableObject {
+    static let shared = NewsFeedService()
+    
+    private let baseURL = "https://native-leon.globo.com"
+    
+    func fetchFeed() -> AnyPublisher<NewsFeedResponse, Error> {
+        guard let url = URL(string: "\(baseURL)/feed/g1") else {
+            return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
+        }
+        
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: NewsFeedResponse.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
