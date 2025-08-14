dfa4f9c7220dbd177788d2927ad2ca4fe68500d4
Neto Moura <Neto Moura>
2025-08-14 17:09:40 -0300

    atualiza NewsFeedApp para exibir a tela NewsFeedView

--- NewsFeed/NewsFeed/NewsFeedApp.swift
+++ NewsFeed/NewsFeed/NewsFeedApp.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 @main
-struct NewsFeedApp: App {
+struct NewsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NewsFeedView()
         }
     }
 }
