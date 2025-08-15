e02ff78ffb791144c4f1320ef973badf93d12d3b
Neto Moura <Neto Moura>
2025-08-15 14:36:48 -0300

    renomeia NewsResponse para NewsModels

--- NewsFeed/NewsFeed/Models/NewsModels.swift
+++ NewsFeed/NewsFeed/Models/NewsModels.swift
@@ -1,5 +1,5 @@
 //
-//  NewsResponse.swift
+//  NewsModels.swift
 //  NewsFeed
 //
 //  Created by Neto Moura on 14/08/25.
@@ -12,33 +12,39 @@ struct NewsResponse: Codable {
 }
 
 struct Feed: Codable {
-    let oferta: String
+    let oferta: String?
     let falkor: Falkor
 }
 
 struct Falkor: Codable {
     let items: [NewsItem]
+    let nextPage: Int
 }
 
 struct NewsItem: Codable, Identifiable {
-    let id: String
-    let type: String
+    let id: String?
+    let type: String?
     let content: NewsContent?
     let metadata: String?
-    
+        
     var isValid: Bool {
         type == "basico" || type == "materia"
     }
+
 }
 
 struct NewsContent: Codable {
-    let title: String?
-    let summary: String?
-    let url: String?
-    let chapeu: String?
+    let chapeu: Chapeu?
     let image: NewsImage?
+    let summary: String?
+    let title: String?
+    let url: String?
+}
+
+struct Chapeu: Codable {
+    let label: String?
 }
 
 struct NewsImage: Codable {
-    let url: String
+    let url: String?
 }
