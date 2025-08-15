ca3fd3619ae52a753034eddb4a5e06302c6a5dba
Neto Moura <Neto Moura>
2025-08-15 01:35:59 -0300

    adiciona MainTabView para selecionar a aba

---
+++ NewsFeed/NewsFeed/Views/MainTabView.swift
@@ -0,0 +1,26 @@
+//
+//  MainTabView.swift
+//  NewsFeed
+//
+//  Created by Neto Moura on 14/08/25.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            NewsListView(feedType: .g1)
+                .tabItem {
+                    Image(systemName: "newspaper")
+                    Text("G1")
+                }
+            
+            NewsListView(feedType: .agronegocio)
+                .tabItem {
+                    Image(systemName: "leaf")
+                    Text("Agronegócio")
+                }
+        }
+    }
+}
